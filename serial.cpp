7fdf7ab2a9973e584c62a340286f55071a00128d
rlkenny98 <rlkenny@csu.fullerton.edu>
2020-10-27 16:54:20 -0700

    Ok

--- serial.cpp
+++ serial.cpp
@@ -4,9 +4,28 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
-int main(){
+int main()
+{
+  string urls[2];
+  int a = 0;
+  ifstream newFile("urls.txt");
+  //newFile.open("urls.txt");
+  if (!newFile)
+  {
+    cout<<"Error opening urls.txt"<< endl;
+    system("pause");
+    return -1;
+  }
+
+  while(!newFile.eof()){
+    getline(newFile,urls[a], '\n');
+    if (a < 3){
+    cout << "Reading urls from file..." << endl;
+    cout << urls[a] << "\n" << endl;}
+  }
 
   /***********TO-DO***********
   1. Create child process
@@ -16,10 +35,12 @@ int main(){
   3. The parent executes a wait() system call until the child exits.
   4. The parent forks off another child process which downloads the next file specified in
      urls.txt.
-  5. Repeat the above steps until all files are downloaded 
-
-
+  5. Repeat the above steps until all files are downloaded
   */
+
+
+
+  pid_t pid;
 
   return 0;
 }
